c1047a0b8b8dc96d95102fffc4177693de74eb8e
Daniel <38067056+p1u3o@users.noreply.github.com>
2023-10-10 15:11:04 +0100

    Create ms-fonts.sh

---
+++ config/scripts/ms-fonts.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+wget https://gist.githubusercontent.com/ervinb/f5042369a1447fedc804a09d87e60997/raw/msttcorefonts-2.5-1.spec
+rpmbuild -bb msttcorefonts-2.5-1.spec
+rpm-ostree install ~/rpmbuild/RPMS/noarch/msttcorefonts-2.5-1.noarch.rpm
