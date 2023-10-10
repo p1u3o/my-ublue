f3a26d647a9385feb2b1f0b0380a196fac13a02a
Daniel <38067056+p1u3o@users.noreply.github.com>
2023-10-10 16:15:08 +0100

    Update ms-fonts.sh

--- config/scripts/ms-fonts.sh
+++ config/scripts/ms-fonts.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf /root/rpmbuild
-wget https://gist.githubusercontent.com/ervinb/f5042369a1447fedc804a09d87e60997/raw/msttcorefonts-2.5-1.spec
-rpmbuild -bb msttcorefonts-2.5-1.spec
-mv /root/rpmbuild/RPMS/noarch/msttcorefonts-2.5-1.noarch.rpm /tmp/
-rm -rf /root/rpmbuild
-rpm-ostree install /tmp/msttcorefonts-2.5-1.noarch.rpm
+rpm-ostree install https://rpmfind.net/linux/sourceforge/m/ms/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
