e9323d33c814e754a2b925c5a82cfcbcfd480ece
Daniel <38067056+p1u3o@users.noreply.github.com>
2023-10-10 16:05:42 +0100

    Update ms-fonts.sh

--- config/scripts/ms-fonts.sh
+++ config/scripts/ms-fonts.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
 rm -rf /root/rpmbuild
 wget https://gist.githubusercontent.com/ervinb/f5042369a1447fedc804a09d87e60997/raw/msttcorefonts-2.5-1.spec
 rpmbuild -bb msttcorefonts-2.5-1.spec
