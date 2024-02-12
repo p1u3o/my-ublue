73d89dc0fe87f170536a811262b6d4e6610edfb9
Daniel <38067056+p1u3o@users.noreply.github.com>
2024-02-12 08:58:17 +0000

    Create prompt.sh

---
+++ config/scripts/prompt.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+wget https://copr.fedorainfracloud.org/coprs/kylegospo/prompt/repo/fedora-$(rpm -E %fedora)/kylegospo-prompt-fedora-$(rpm -E %fedora).repo?arch=x86_64 -O /etc/yum.repos.d/_copr_kylegospo-prompt.repo
+
+rpm-ostree override replace \
+--experimental \
+--from repo=copr:copr.fedorainfracloud.org:kylegospo:prompt \
+    vte291 \
+    vte-profile
+rpm-ostree install \
+    prompt
+    
+rm -f /etc/yum.repos.d/_copr_kylegospo-prompt.repo
