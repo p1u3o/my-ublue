86a038770b066e10c68f675a0d46c22c166b4a7b
Daniel <38067056+p1u3o@users.noreply.github.com>
2024-02-12 09:25:49 +0000

    Create other.sh

---
+++ config/scripts/other.sh
@@ -0,0 +1,10 @@
+RUN pip install --prefix=/usr yafti && \
+    pip install --prefix=/usr topgrade && \
+    mkdir -p /usr/etc/flatpak/remotes.d && \
+    wget -q https://dl.flathub.org/repo/flathub.flatpakrepo -P /usr/etc/flatpak/remotes.d && \
+    sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf && \
+    sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf && \
+    echo "Hidden=true" >> /usr/share/applications/fish.desktop && \
+    echo "Hidden=true" >> /usr/share/applications/htop.desktop && \
+    echo "Hidden=true" >> /usr/share/applications/nvtop.desktop && \
+    echo "Hidden=true" >> /usr/share/applications/gnome-system-monitor.desktop
