0cb80169b077532a26d6bb0327148b90827bc448
Daniel <38067056+p1u3o@users.noreply.github.com>
2024-02-12 09:49:26 +0000

    Update other.sh

--- config/scripts/other.sh
+++ config/scripts/other.sh
@@ -1,10 +1,13 @@
-RUN pip install --prefix=/usr yafti && \
-    pip install --prefix=/usr topgrade && \
-    mkdir -p /usr/etc/flatpak/remotes.d && \
-    wget -q https://dl.flathub.org/repo/flathub.flatpakrepo -P /usr/etc/flatpak/remotes.d && \
-    sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf && \
-    sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf && \
-    echo "Hidden=true" >> /usr/share/applications/fish.desktop && \
-    echo "Hidden=true" >> /usr/share/applications/htop.desktop && \
-    echo "Hidden=true" >> /usr/share/applications/nvtop.desktop && \
-    echo "Hidden=true" >> /usr/share/applications/gnome-system-monitor.desktop
+#!/usr/bin/env bash
+
+pip install --prefix=/usr yafti
+pip install --prefix=/usr topgrade
+
+mkdir -p /usr/etc/flatpak/remotes.d
+wget -q https://dl.flathub.org/repo/flathub.flatpakrepo -P /usr/etc/flatpak/remotes.d
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf 
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf
+echo "Hidden=true" >> /usr/share/applications/fish.desktop
+echo "Hidden=true" >> /usr/share/applications/htop.desktop 
+echo "Hidden=true" >> /usr/share/applications/nvtop.desktop 
+echo "Hidden=true" >> /usr/share/applications/gnome-system-monitor.desktop
