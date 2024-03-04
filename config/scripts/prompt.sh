bc1f68b62b4e5929ffdf9a9fa32af35dff4972e5
Daniel <38067056+p1u3o@users.noreply.github.com>
2024-03-04 08:54:54 +0000

    Update prompt.sh

--- config/scripts/prompt.sh
+++ config/scripts/prompt.sh
@@ -5,7 +5,8 @@ rpm-ostree override replace \
 --experimental \
 --from repo=copr:copr.fedorainfracloud.org:kylegospo:prompt \
     vte291 \
-    vte-profile
+    vte-profile \
+    libadwaita 
 rpm-ostree install \
     ptyxis
     
