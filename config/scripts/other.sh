c7b90a417d37a8758fd156c72242e33de037e1c9
Pluto <38067056+p1u3o@users.noreply.github.com>
2024-04-03 13:18:28 +0100

    update

--- config/scripts/other.sh
+++ config/scripts/other.sh
@@ -11,3 +11,8 @@ echo "Hidden=true" >> /usr/share/applications/fish.desktop
 echo "Hidden=true" >> /usr/share/applications/htop.desktop 
 echo "Hidden=true" >> /usr/share/applications/nvtop.desktop 
 echo "Hidden=true" >> /usr/share/applications/gnome-system-monitor.desktop
+
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz" 
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
