6d236652952b204a1184f894dead84e66892caf5
Daniel <38067056+p1u3o@users.noreply.github.com>
2024-05-13 11:13:17 +0100

    Update prompt.sh

--- config/scripts/prompt.sh
+++ config/scripts/prompt.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-wget https://copr.fedorainfracloud.org/coprs/kylegospo/prompt/repo/fedora-$(rpm -E %fedora)/kylegospo-prompt-fedora-$(rpm -E %fedora).repo?arch=x86_64 -O /etc/yum.repos.d/_copr_kylegospo-prompt.repo
 curl -Lo /etc/yum.repos.d/ublue-os-staging-fedora-$(rpm -E %fedora).repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo
 
 rpm-ostree override replace \
