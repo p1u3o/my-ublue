6391e998170b5887ef6a818c9839f83985cef0fc
Daniel <38067056+p1u3o@users.noreply.github.com>
2024-05-13 11:13:07 +0100

    Update prompt.sh

--- config/scripts/prompt.sh
+++ config/scripts/prompt.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 wget https://copr.fedorainfracloud.org/coprs/kylegospo/prompt/repo/fedora-$(rpm -E %fedora)/kylegospo-prompt-fedora-$(rpm -E %fedora).repo?arch=x86_64 -O /etc/yum.repos.d/_copr_kylegospo-prompt.repo
+curl -Lo /etc/yum.repos.d/ublue-os-staging-fedora-$(rpm -E %fedora).repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo
 
 rpm-ostree override replace \
 --experimental \
---from repo=copr:copr.fedorainfracloud.org:kylegospo:prompt \
+--from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
     vte291 \
-    vte-profile \
-    libadwaita 
-rpm-ostree install \
-    ptyxis
-    
-rm -f /etc/yum.repos.d/_copr_kylegospo-prompt.repo
+    vte-profile
+rpm-ostree install ptyxis
