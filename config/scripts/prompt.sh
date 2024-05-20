8051a9ba0467f30231bae1dd28a68d8498907194
Daniel <38067056+p1u3o@users.noreply.github.com>
2024-05-20 10:15:11 +0100

    Update prompt.sh

--- config/scripts/prompt.sh
+++ config/scripts/prompt.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 curl -Lo /etc/yum.repos.d/ublue-os-staging-fedora-$(rpm -E %fedora).repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo
 
-rpm-ostree override replace \
---experimental \
---from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-    vte291 \
-    vte-profile
-rpm-ostree install ptyxis
+#rpm-ostree override replace \
+#--experimental \
+#--from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+#    vte291 \
+#    vte-profile
+#rpm-ostree install ptyxis
